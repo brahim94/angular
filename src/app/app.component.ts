af43e6bcd5abd2101a107fbf25c29997306bfde1
HimDev <brahim-ayad@hotmail.com>
2019-07-29 22:10:06 +0100

    On one Off One

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isAuth = false;
+
+  appareils: any[];
+
+
 
   lastUpdate = new Promise((resolve, reject) => {
     const date = new Date();
@@ -17,31 +21,32 @@ export class AppComponent {
     );
   });
 
-  appareils = [
-  {
-  name: 'Machine à laver',
-  status: 'éteint'
-  },
-  {
-  name: 'Frigo',
-  status: 'allumé'
-  },
-  {
-  name: 'Ordinateur',
-  status: 'éteint'
-  }
-  ];
 
 
-
-  constructor() {
+  constructor(private appareilService: AppareilService) {
   setTimeout(
   () => {
   this.isAuth = true;
   }, 4000
   );
   }
-  onEtteint() {
-    console.log('On allume tout !');
-}
+
+  ngOnInit() {
+
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllumer() {
+  this.appareilService.switchOnAll();
+  }
+
+  onEteindre() {
+    if(confirm('Etes-vous sùr de vouloir éteindre tous vos appareils ?')) {
+    this.appareilService.switchOffAll();
+        } else {
+      return null;
+
+            }
+                }
+
 }
